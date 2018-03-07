b85ef1544518d729221fa1cc1a489871b137b46f
vladbusov <to2002to2002@gmail.com>
2018-03-07 15:32:42 +0300

    first full 0.2 alpha version of $

---
+++ src/main/java/main/controllers/UserApiConroller.java
@@ -0,0 +1,47 @@
+package main.controllers;
+import main.models.User;
+import main.models.Message;
+import main.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+
+
+@RestController
+public class UserApiConroller {
+
+    // Post Mapping
+
+    @PostMapping(value = "/signup", produces = "application/json")
+    public Message register(@RequestBody User newbie, HttpSession session) {
+        return UserService.registUser(newbie);
+    }
+
+    @PostMapping(value = "/signin", produces = "application/json")
+    public Message authorize(@RequestBody String email, @RequestBody String password, HttpSession session) {
+        return UserService.login(email,password,session);
+    }
+
+    @PostMapping(value = "/edit", produces = "application/json")
+    public Message editProfile(@RequestBody User user, HttpSession session) {
+        return UserService.editUser(user,session);
+    }
+
+    @PostMapping(value = "/logout", produces = "application/json")
+    public Message logout(HttpSession session) {
+        return UserService.loguot(session);
+    }
+
+    // Get Mapping
+
+    @GetMapping(value = "/user", produces = "application/json")
+    public Message getUser(HttpSession session) {
+        return UserService.getUserData(session);
+    }
+
+    @GetMapping(value = "/{unknown}", produces = "application/json")
+    public Message register(@PathVariable("unknown") String unknown , HttpSession session) {
+        return UserService.notFound(unknown);
+    }
+
+}
